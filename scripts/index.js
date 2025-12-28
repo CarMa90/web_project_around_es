5a9f9f8b009037ac167f8f9f439baa580812c2fc
CarMa90 <el.matteotti@gmail.com>
2025-12-27 17:01:17 -0700

    feature profile popup

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,51 @@ let initialCards = [
 initialCards.forEach(function (card) {
   console.log(card["name"]);
 });
+
+const d = document;
+const profileEditBtn = d.querySelector(".profile__edit-button");
+const profileEditPopup = d.querySelector("#edit-popup");
+const profileClosePopupBtn = profileEditPopup.querySelector(".popup__close");
+const profileTitle = d.querySelector(".profile__title");
+const profileDescription = d.querySelector(".profile__description");
+const inputName = profileEditPopup.querySelector(".popup__input_type_name");
+const inputDescription = profileEditPopup.querySelector(
+  ".popup__input_type_description"
+);
+const formElement = d.querySelector("#edit-profile-form");
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  inputName.value = profileTitle.textContent;
+  inputDescription.value = profileDescription.textContent;
+}
+function handleOpenEditModal(modal) {
+  openModal(modal);
+  fillProfileForm();
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closeModal(profileEditPopup);
+}
+
+profileEditBtn.addEventListener("click", (e) => {
+  // console.log(e.target);
+  handleOpenEditModal(profileEditPopup);
+});
+
+profileClosePopupBtn.addEventListener("click", (e) => {
+  // console.log(e.target);
+  closeModal(profileEditPopup);
+});
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
